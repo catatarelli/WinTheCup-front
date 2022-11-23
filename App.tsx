c771a5540d5a76f5d6b38489e66b4d9b431b6743
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-23 13:51:40 +0100

    Add background image

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
+import RegisterScreen from "./src/screens/RegisterScreen/RegisterScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <RegisterScreen />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
     justifyContent: "center",
   },
 });
---
+++ src/styles/colors.styles.ts
@@ -0,0 +1,9 @@
+const colors = {
+  grey: "#3F3F3F",
+  beige: "#EEEEE4",
+  wine: "#8A1538",
+  white: "#fff",
+  black: "#000",
+};
+
+export default colors;
