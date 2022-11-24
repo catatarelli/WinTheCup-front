672cc04b3c66dfbc230385e606abde3ca245b9db
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-24 12:14:29 +0100

    Add userSlice

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import loginUserReducer from "./reducers/loginUserReducer/loginUserReducer";
+import logoutUserReducer from "./reducers/logoutUserReducer/logoutUserReducer";
+import { UserState } from "./userTypes";
+
+export const userInitialState: UserState = {
+  id: "",
+  token: "",
+  username: "",
+  isLogged: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: userInitialState,
+  reducers: {
+    loginUserReducer,
+    logoutUserReducer,
+  },
+});
+
+export const userReducer = userSlice.reducer;
+export const {
+  loginUserReducer: loginUserActionCreator,
+  logoutUserReducer: logoutUserActionCreator,
+} = userSlice.actions;
