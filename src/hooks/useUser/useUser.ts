de3ace2a8893b78c02670dd35f9e4ab98aa4037f
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-24 17:18:10 +0100

    Add loginUser to useUser custom hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { REACT_APP_API_URL } from "@env";
 import { openModalActionCreator } from "../../redux/features/uiSlice/uiSlice";
 import { useAppDispatch } from "../../redux/hooks";
-import { RegisterData } from "../../redux/features/user/userTypes";
+import type {
+  LoginResponse,
+  RegisterData,
+  UserCredentials,
+} from "../../redux/features/user/userTypes";
+import { type JwtCustomPayload } from "../../types/types";
+import decodeToken from "../../utils/decodeToken";
+import { loginUserActionCreator } from "../../redux/features/user/userSlice";
 
 const useUser = () => {
   const dispatch = useAppDispatch();
@@ -26,7 +34,34 @@ const useUser = () => {
     }
   };
 
-  return { registerUser };
+  const loginUser = async (userData: UserCredentials) => {
+    try {
+      const responseData = await axios.post<LoginResponse>(
+        `${REACT_APP_API_URL}/user/login`,
+        userData
+      );
+      if (responseData.status === 401) {
+        dispatch(
+          openModalActionCreator({ modal: "Wrong credentials", isError: true })
+        );
+      }
+
+      const { token } = responseData.data;
+      const loggedUser: JwtCustomPayload = decodeToken(token);
+
+      dispatch(loginUserActionCreator({ ...loggedUser, token }));
+      await AsyncStorage.setItem("token", token);
+    } catch {
+      dispatch(
+        openModalActionCreator({
+          modal: "It is not possible to login",
+          isError: true,
+        })
+      );
+    }
+  };
+
+  return { registerUser, loginUser };
 };
 
 export default useUser;
