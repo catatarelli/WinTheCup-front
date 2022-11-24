8d63d12be3924bde30f062e9a2f5935b64565c1d
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-24 13:53:29 +0100

    Add useToken custom hook

---
+++ src/hooks/useToken/useToken.ts
@@ -0,0 +1,24 @@
+import { useCallback } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { loginUserActionCreator } from "../../redux/features/user/userSlice";
+import { useAppDispatch } from "../../redux/hooks";
+import decodeToken from "../../utils/decodeToken";
+
+const useToken = () => {
+  const dispatch = useAppDispatch();
+
+  const checkToken = useCallback(async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      const user = decodeToken(token);
+      dispatch(loginUserActionCreator({ ...user, token }));
+    }
+  }, [dispatch]);
+
+  const removeToken = () => {
+    AsyncStorage.removeItem("token");
+  };
+  return { checkToken, removeToken };
+};
+
+export default useToken;
