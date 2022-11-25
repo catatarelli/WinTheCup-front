fc2b65674b00b2f559378cf01384cdf855410eed
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-25 10:32:50 +0100

    Modify closeModal and openModal tests

--- src/redux/features/uiSlice/reducers/closeModalReducer/closeModalReducer.test.ts
+++ src/redux/features/uiSlice/reducers/closeModalReducer/closeModalReducer.test.ts
@@ -1,11 +1,12 @@
 import { closeModalActionCreator, uiReducer } from "../../uiSlice";
 
-describe("Given closeModalReducer", () => {
+describe("Given a closeModalReducer", () => {
   describe("When it recieves an initial state with 'Register was successful'", () => {
     test("Then it should return a new state with an empty modal", () => {
       const currentUiState = {
         modal: "Register was successful",
         isError: false,
+        isLoading: false,
       };
 
       const expectedUiState = { ...currentUiState, modal: "" };
--- src/redux/features/uiSlice/reducers/openModalReducer/openModalReducer.test.ts
+++ src/redux/features/uiSlice/reducers/openModalReducer/openModalReducer.test.ts
@@ -1,13 +1,14 @@
 import { openModalActionCreator, uiReducer } from "../../uiSlice";
 
-describe("Given openModalReducer", () => {
+describe("Given an openModalReducer", () => {
   describe("When it recieves an initial state and a payload with 'Register was successful'", () => {
     test("Then it should return a new state with the modal's value 'Register was successful' ", () => {
-      const currentUiState = { modal: "", isError: false };
+      const currentUiState = { modal: "", isError: false, isLoading: false };
 
       const payload = {
         modal: "Register was successful",
         isError: false,
+        isLoading: false,
       };
       const expectedUiState = {
         ...currentUiState,
