7690eb1d82f0289ab852f0b4d364aaffeb00cf1b
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-25 10:34:18 +0100

    Add Loading styles

---
+++ src/components/Loading/LoadingStyled.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  activityIndicatorWrapper: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gif: {
+    height: 150,
+    width: 150,
+  },
+  modal: {
+    zIndex: 1100,
+  },
+});
+
+export default styles;
