34eeef9810ad5c1d7b9569d94ac88da5a4d6a12b
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-25 22:21:42 +0100

    Add NavigationContainer to makeWrapper

--- src/mocks/makeWrapper.tsx
+++ src/mocks/makeWrapper.tsx
@@ -1,9 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 
 const makeWrapper = ({ children }: { children: JSX.Element }) => (
-  <Provider store={store}>{children}</Provider>
+  <Provider store={store}>
+    <NavigationContainer>{children}</NavigationContainer>
+  </Provider>
 );
 
 export default makeWrapper;
