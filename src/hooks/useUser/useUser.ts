3c1ca92b0e250ae2f0e4d225cd86f5a854030934
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-26 16:43:56 +0100

    Add getPredictions to custom hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,23 +1,35 @@
-import axios from "axios";
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/naming-convention */
+import axios, { AxiosError } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { REACT_APP_API_URL } from "@env";
-import { openModalActionCreator } from "../../redux/features/ui/uiSlice";
-import { useAppDispatch } from "../../redux/hooks";
+import {
+  hideLoadingActionCreator,
+  openModalActionCreator,
+  showLoadingActionCreator,
+} from "../../redux/features/ui/uiSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import type {
   LoginResponse,
+  PredictionsResponse,
   RegisterData,
   UserCredentials,
 } from "../../redux/features/user/userTypes";
 import { type JwtCustomPayload } from "../../types/types";
 import decodeToken from "../../utils/decodeToken";
-import { loginUserActionCreator } from "../../redux/features/user/userSlice";
+import {
+  loadPredictionsActionCreator,
+  loginUserActionCreator,
+} from "../../redux/features/user/userSlice";
 import { useNavigation } from "@react-navigation/native";
 import type { LoginScreenNavigationProp } from "../../types/navigation.types";
 import Routes from "../../navigation/routes";
+import { useCallback } from "react";
 
 const useUser = () => {
   const dispatch = useAppDispatch();
   const navigation = useNavigation<LoginScreenNavigationProp>();
+  const { token } = useAppSelector((state) => state.user);
 
   const registerUser = async (userData: RegisterData) => {
     try {
@@ -73,7 +85,39 @@ const useUser = () => {
     }
   };
 
-  return { registerUser, loginUser };
+  const getPredictions = useCallback(async () => {
+    try {
+      dispatch(showLoadingActionCreator());
+
+      const response = await axios.get<PredictionsResponse>(
+        `${REACT_APP_API_URL}/predictions`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const apiResponse = response.data;
+
+      dispatch(loadPredictionsActionCreator(apiResponse.predictions));
+      dispatch(hideLoadingActionCreator());
+    } catch (error: unknown) {
+      dispatch(hideLoadingActionCreator());
+      if (error instanceof AxiosError) {
+        dispatch(
+          openModalActionCreator({
+            isError: true,
+            modal: (error as AxiosError<{ error: string }>).response?.data
+              .error!,
+            isLoading: false,
+          })
+        );
+      }
+    }
+  }, [dispatch]);
+
+  return { registerUser, loginUser, getPredictions };
 };
 
 export default useUser;
