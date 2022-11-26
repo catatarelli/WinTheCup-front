f61f62af48cf5b7db6a20ebc434265fac7318470
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-26 09:39:50 +0100

    Change navigation in app

--- App.tsx
+++ App.tsx
@@ -1,38 +1,12 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 import { store } from "./src/redux/store";
 import { Provider } from "react-redux";
-import Routes from "./src/navigation/routes";
-import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
-import RegisterScreen from "./src/screens/RegisterScreen/RegisterScreen";
-import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
+import StackNavigatorExplorer from "./src/navigation/StackNavigator";
 
-const App = () => {
-  const Stack = createNativeStackNavigator();
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={Routes.welcome}
-            component={WelcomeScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name={Routes.register}
-            component={RegisterScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name={Routes.login}
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <StackNavigatorExplorer />
+  </Provider>
+);
 
 export default App;
