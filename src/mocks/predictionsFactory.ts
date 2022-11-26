e232d5197cf400d2f8ccaccc1b0c99663941aed1
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-26 23:20:49 +0100

    Create predictionsFactory

--- src/mocks/predictionsFactory.ts
+++ src/mocks/predictionsFactory.ts
@@ -1,19 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Factory } from "fishery";
 import { faker } from "@faker-js/faker";
 import type { PredictionStructure } from "../redux/features/user/userTypes";
-import type { ImageSourcePropType } from "react-native";
 
 const predictionFactory = Factory.define<PredictionStructure>(() => ({
   match: "Argentina vs England",
-  goalsTeam1: faker.datatype.number(),
-  goalsTeam2: faker.datatype.number(),
-  redCards: faker.datatype.number(),
-  yellowCards: faker.datatype.number(),
-  penalties: faker.datatype.number(),
-  picture: faker.image.sports() as ImageSourcePropType,
+  goalsTeam1: faker.datatype.number(9),
+  goalsTeam2: faker.datatype.number(9),
+  redCards: faker.datatype.number(10),
+  yellowCards: faker.datatype.number(10),
+  penalties: faker.datatype.number(10),
+  picture: require("../../assets/argentinafans.jpg"),
   backupPicure: faker.image.sports(),
 }));
 
