0722c1305fb19dce07475ad8f8af7aba8092fb11
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-27 10:42:14 +0100

    Add case to useToken test

--- src/hooks/useToken/useToken.test.ts
+++ src/hooks/useToken/useToken.test.ts
@@ -67,13 +67,31 @@ describe("Given the hook useToken", () => {
         wrapper: makeWrapper,
       });
 
-      AsyncStorage.removeItem = jest.fn().mockReturnValue(token);
+      AsyncStorage.getItem = jest.fn().mockReturnValue(token);
 
       await checkToken();
 
       expect(dispatchSpy).toHaveBeenCalledWith(
         loginUserActionCreator(mockUser)
       );
+    });
+  });
+
+  describe("When its method checkToken is invoked and there is no token in AsyncStorage", () => {
+    test("Then it should not call the dispatch", async () => {
+      const {
+        result: {
+          current: { checkToken },
+        },
+      } = renderHook(() => useToken(), {
+        wrapper: makeWrapper,
+      });
+
+      AsyncStorage.getItem = jest.fn().mockReturnValue("");
+
+      await checkToken();
+
+      expect(dispatchSpy).not.toHaveBeenCalled();
     });
   });
 
