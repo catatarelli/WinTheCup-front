8b5274bbd4a07b0b0a9c469a003a767a3a21e099
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-27 21:49:39 +0100

    Add navigation to detail to PredictionCard

--- src/components/PredictionCard/PredictionCard.test.tsx
+++ src/components/PredictionCard/PredictionCard.test.tsx
@@ -1,8 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { render, screen } from "@testing-library/react-native";
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { fireEvent, screen } from "@testing-library/react-native";
 import React from "react";
 import { getRandomPrediction } from "../../mocks/predictionsFactory";
+import { renderWithProviders } from "../../mocks/renderWithProviders";
+import Routes from "../../navigation/routes";
 import PredictionCard from "./PredictionCard";
+
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+}));
+
+const mockedNavigate = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
 
 describe("Given a PredictionCard component", () => {
   describe("When it's rendered with a prediction", () => {
@@ -10,13 +31,27 @@ describe("Given a PredictionCard component", () => {
       const prediction = getRandomPrediction();
       const imageId = "predictionPicture";
 
-      render(<PredictionCard prediction={prediction} />);
+      renderWithProviders(<PredictionCard prediction={prediction} />);
 
       const displayedImage = screen.queryByTestId(imageId);
       const displayedMatch = screen.queryByText(prediction.match);
 
       expect(displayedImage).toBeDefined();
       expect(displayedMatch).toBeDefined();
+    });
+  });
+
+  describe("And the user presses on the match text", () => {
+    test("Then the useNavigation should be called with the detail page reference", async () => {
+      const prediction = getRandomPrediction();
+      const matchButtonId = "match";
+
+      renderWithProviders(<PredictionCard prediction={prediction} />);
+
+      const matchButton = await screen.queryByTestId(matchButtonId);
+      fireEvent(matchButton, "press");
+
+      expect(mockedNavigate).toHaveBeenCalledWith(Routes.predictionDetail);
     });
   });
 });
