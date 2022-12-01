d33a2eb3afb6bd7609165b809454c943e781a017
Catalina Tarelli <catatarelli@hotmail.com>
2022-12-01 16:04:59 +0100

    Add Tabnaviagor test case of logout

--- src/navigation/TabNavigator/TabNavigator.test.tsx
+++ src/navigation/TabNavigator/TabNavigator.test.tsx
@@ -1,16 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
 import TabNavigator from "./TabNavigator";
-import { screen, render } from "@testing-library/react-native";
+import { screen, render, fireEvent } from "@testing-library/react-native";
 import { Provider } from "react-redux";
 import { mockInitialStoreSuccessModal } from "../../mocks/mockInitialStore";
 import { NavigationContainer } from "@react-navigation/native";
+import Routes from "../routes";
 
 jest.mock("@react-native-async-storage/async-storage", () => ({
   setItem: jest.fn(),
   getItem: jest.fn(),
   removeItem: jest.fn(),
 }));
+
+const mockedNavigate = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
 
 describe("Given a TabNavigator component", () => {
   describe("When it's rendered", () => {
@@ -30,6 +44,25 @@ describe("Given a TabNavigator component", () => {
       );
 
       expect(displayedtabNavigationMyPredictions).toBeDefined();
+    });
+  });
+
+  describe("And the user presses the logout button", () => {
+    test("Then the useNavigation should be called with the Login page reference", async () => {
+      const logoutButtonText = "Log out";
+
+      render(
+        <Provider store={mockInitialStoreSuccessModal}>
+          <NavigationContainer>
+            <TabNavigator />
+          </NavigationContainer>
+        </Provider>
+      );
+
+      const logoutButton = await screen.queryByText(logoutButtonText);
+      fireEvent(logoutButton, "press");
+
+      expect(mockedNavigate).toHaveBeenCalledWith(Routes.login);
     });
   });
 });
