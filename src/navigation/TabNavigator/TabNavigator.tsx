ac2b68ab852414a5e544068f4d797dee06ea832e
Catalina Tarelli <catatarelli@hotmail.com>
2022-12-01 15:52:13 +0100

    Add logout button to navigation tab

--- src/navigation/TabNavigator/TabNavigator.tsx
+++ src/navigation/TabNavigator/TabNavigator.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { Image } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCirclePlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import icon from "../../../assets/magic-ball.png";
 import MyPredictionsScreen from "../../screens/MyPredictionsScreen/MyPredictionsScreen";
 import Routes from "../routes";
@@ -11,9 +15,24 @@ import colors from "../../styles/colors.styles";
 import navigationStyles from "./TabNavigatorStyled";
 import PredictionDetailScreen from "../../screens/PredictionDetailScreen/PredictionDetailScreen";
 import CreatePredictionScreen from "../../screens/CreatePredictionScreen/CreatePredictionScreen";
+import { logoutUserActionCreator } from "../../redux/features/user/userSlice";
+import { useAppDispatch } from "../../redux/hooks";
+import useToken from "../../hooks/useToken/useToken";
+import { useNavigation } from "@react-navigation/native";
+import { type LoginScreenNavigationProp } from "../../types/navigation.types";
+import CustomModal from "../../components/CustomModal/CustomModal";
 
 const TabNavigator = (): JSX.Element => {
   const Tab = createBottomTabNavigator();
+  const dispatch = useAppDispatch();
+  const { removeToken } = useToken();
+  const navigation = useNavigation<LoginScreenNavigationProp>();
+
+  const handleLogout = () => {
+    removeToken();
+    dispatch(logoutUserActionCreator());
+    navigation.navigate(Routes.login);
+  };
 
   return (
     <Tab.Navigator
@@ -56,6 +75,23 @@ const TabNavigator = (): JSX.Element => {
           title: "Create prediction",
           tabBarIcon: ({ color }) => (
             <FontAwesomeIcon icon={faCirclePlus} size={46} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={Routes.logout}
+        listeners={{
+          tabPress: handleLogout,
+        }}
+        component={CustomModal}
+        options={{
+          title: "Log out",
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon
+              icon={faRightFromBracket}
+              size={46}
+              color={color}
+            />
           ),
         }}
       />
--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -6,6 +6,7 @@ enum Routes {
   predictionDetail = "PredictionDetail",
   home = "Home",
   createPrediction = "CreatePrediction",
+  logout = "Logout",
 }
 
 export default Routes;
