834c00f1555ef3636a2140fde96bb4f00094e8c4
Catalina Tarelli <catatarelli@hotmail.com>
2022-12-02 09:33:23 +0100

    Add filter so that select doesn't show matches with a prediction

--- src/screens/CreatePredictionScreen/CreatePredictionScreen.tsx
+++ src/screens/CreatePredictionScreen/CreatePredictionScreen.tsx
@@ -3,12 +3,23 @@ import React from "react";
 import screenStyles from "../../styles/screen.styles";
 import CreatePredictionForm from "../../components/CreatePredictionForm/CreatePredictionForm";
 import headingStyles from "../../styles/headings.styles";
+import { useAppSelector } from "../../redux/hooks";
+import matches from "../../utils/matches";
 
-const CreatePredictionScreen = (): JSX.Element => (
-  <SafeAreaView style={screenStyles.container}>
-    <Text style={headingStyles.pageTitle}>Create a new prediction</Text>
-    <CreatePredictionForm />
-  </SafeAreaView>
-);
+const CreatePredictionScreen = (): JSX.Element => {
+  const { predictions } = useAppSelector((state) => state.predictions);
+
+  const matchesList = matches.filter(
+    (match) =>
+      !predictions.some((prediction) => prediction.match === match.value)
+  );
+
+  return (
+    <SafeAreaView style={screenStyles.container}>
+      <Text style={headingStyles.pageTitle}>Create a new prediction</Text>
+      <CreatePredictionForm matches={matchesList} />
+    </SafeAreaView>
+  );
+};
 
 export default CreatePredictionScreen;
